a7d84c92f6bbedcfa44ddd7118c376f3015a313f
Pete Moore <pmoore@mozilla.com>
2018-06-15 13:07:19 -0700

    wip

--- runasadministrator.go
+++ runasadministrator.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/taskcluster/taskcluster-base-go/scopes"
 )
 
@@ -43,9 +46,27 @@ func (l *RunAsAdministratorTask) RequiredScopes() scopes.Required {
 	}}
 }
 
+type RunAsAdministratorHandler struct {
+}
+
 func (l *RunAsAdministratorTask) Start() *CommandExecutionError {
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        &RunAsAdministratorHandler{},
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	go ProcessRequests(s)
 	return nil
 }
 
+func ProcessRequests(s *http.Server) {
+	// s.ListenAndServe()
+}
+
 func (l *RunAsAdministratorTask) Stop(err *ExecutionErrors) {
+}
+
+func (handler *RunAsAdministratorHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 }
